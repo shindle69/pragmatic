e1ad2e2d9c78e9d9677a9bbe6d70b8560d4cbe9c
unknown <juyouso@gmail.com>
2020-12-04 13:15:35 +0900

    Chapter23 Naverfont

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,15 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp import views
+from accountapp.views import AccountCreateView
 
 app_name = "accountapp"
 
 urlpatterns =[
     path('hello_world/',views.hello_world,name='hello_world'),
+    path('login/',LoginView.as_view(template_name="accountapp/login.html"), name='login'),
+    path('logout/',LogoutView.as_view(), name='logout'),
+
+    path('create/',AccountCreateView.as_view(), name='create'),
 ]
--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -52,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'accountapp',
 ]
 
@@ -140,3 +143,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS =[
     BASE_DIR /"static",
 ]
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')

1d4841a711bf8c4112375052105b5086f8d90169
unknown <juyouso@gmail.com>
2020-12-04 19:15:12 +0900

    Chap28 Decorator

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,18 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
+
+has_ownership = [account_ownership_required, login_required]
+
 
 # Create your views here.
 
-
+@login_required
 def hello_world(request):
     return render(request, 'accountapp/hello_world.html')
-
-
 
 class AccountCreateView(CreateView):
     model = User
@@ -27,6 +31,8 @@ class AccountDetailView(DetailView):
     template_name = 'accountapp/detail.html'
 
 
+@method_decorator(has_ownership,'get')
+@method_decorator(has_ownership,'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
@@ -35,8 +41,12 @@ class AccountUpdateView(UpdateView):
     template_name = 'accountapp/update.html'
 
 
+@method_decorator(has_ownership,'get')
+@method_decorator(has_ownership,'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
+
+

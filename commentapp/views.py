fd4806dbe0f48529311aea7ffdc215f68d8830a8
unknown <juyouso@gmail.com>
2020-12-10 18:01:23 +0900

    commentapp final

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,8 +1,10 @@
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from articleapp.models import Article
+from commentapp.decorators import comment_ownership_required
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
@@ -22,3 +24,12 @@ class CommentCreateView(CreateView):
     def get_success_url(self):
         return reverse('articleapp:detail',kwargs={'pk':self.object.article.pk })
 
+@method_decorator(comment_ownership_required,'get')
+@method_decorator(comment_ownership_required,'post')
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail',kwargs={'pk':self.object.article.pk })

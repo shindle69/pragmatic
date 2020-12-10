4ab63602f5299b9ade900af13c9c2108797948d9
unknown <juyouso@gmail.com>
2020-12-10 15:09:53 +0900

    Chap36 article ListView and Pagination

--- articleapp/admin.py
+++ articleapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from articleapp.models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title','writer','content']
+
+admin.site.register(Article, ArticleAdmin)
